6b8072007d04907c495f73efbf54e0d5074a4f4c
Wang Nan <wangnan0@huawei.com>
2015-06-26 11:26:59 +0000

    perf tools: Add bpf_fd field to evsel and config it
    This patch adds a bpf_fd field to 'struct evsel' then introduces method
    to config it. In bpf-loader, a bpf__foreach_tev() function is added,
    which calls the callback function for each 'struct probe_trace_event'
    events for each bpf program with their file descriptors. In evlist.c,
    perf_evlist__add_bpf() is introduced to add all bpf events into evlist.
    The event names are found from probe_trace_event structure.
    'perf record' calls perf_evlist__add_bpf().
    
    Since bpf-loader.c will not be built if libbpf is turned off, an empty
    bpf__foreach_tev() is defined in bpf-loader.h to avoid compiling
    error.
    
    This patch iterates over 'struct probe_trace_event' instead of
    'struct probe_trace_event' during the loop for further patches, which
    will generate multiple instances form one BPF program and install then
    onto different 'struct probe_trace_event'.
    
    Signed-off-by: Wang Nan <wangnan0@huawei.com>
    Cc: Alexei Starovoitov <ast@plumgrid.com>
    Cc: Brendan Gregg <brendan.d.gregg@gmail.com>
    Cc: Daniel Borkmann <daniel@iogearbox.net>
    Cc: David Ahern <dsahern@gmail.com>
    Cc: He Kuang <hekuang@huawei.com>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Kaixu Xia <xiakaixu@huawei.com>
    Cc: Masami Hiramatsu <masami.hiramatsu.pt@hitachi.com>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Zefan Li <lizefan@huawei.com>
    Cc: pi3orama@163.com
    Cc: Arnaldo Carvalho de Melo <acme@redhat.com>
    Link: http://lkml.kernel.org/n/1436445342-1402-25-git-send-email-wangnan0@huawei.com

--- tools/perf/util/evsel.c
+++ tools/perf/util/evsel.c
@@ -207,6 +207,7 @@ void perf_evsel__init(struct perf_evsel *evsel,
 	evsel->unit	   = "";
 	evsel->scale	   = 1.0;
 	evsel->evlist	   = NULL;
+	evsel->bpf_fd	   = -1;
 	INIT_LIST_HEAD(&evsel->node);
 	INIT_LIST_HEAD(&evsel->config_terms);
 	perf_evsel__object.init(evsel);
